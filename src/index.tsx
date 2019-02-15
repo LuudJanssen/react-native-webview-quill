617d62f69f2f8a3705120b548020a6245f099e5b
luudjanssen <luud-@live.nl>
2019-02-15 17:22:19 +0100

    Run tslint

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 
 export class Hello extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text>Hello from the other side!</Text>
-            </View>
-        );
-    }
+  public render() {
+    return (
+      <View>
+        <Text>Hello from the other side!</Text>
+      </View>
+    );
+  }
 }
