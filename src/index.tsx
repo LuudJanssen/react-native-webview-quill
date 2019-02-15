d7dbc7bc6eaa1ed88018c027391edef0d356d2b0
luudjanssen <luud-@live.nl>
2019-02-15 16:47:34 +0100

    Linting complained...

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export class Hello extends React.Component {
     render() {
