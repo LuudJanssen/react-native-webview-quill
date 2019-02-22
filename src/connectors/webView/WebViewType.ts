bda8bbb647ffa83bef963d16945fb3bd840e9d14
s145469 <luud-@live.nl>
2019-02-22 23:29:05 +0100

    Fix typing

--- src/connectors/webView/WebViewType.ts
+++ src/connectors/webView/WebViewType.ts
@@ -1,3 +1,11 @@
-import { WebView, WebViewProps } from 'react-native';
+import { WebView as ReactNativeWebView, WebViewProps } from 'react-native';
+import { WebView as CommunityWebView } from 'react-native-webview';
 
-export type WebViewType = new (props: WebViewProps) => WebView;
+// We have to do this because react-native webview and react-native's Uri Source schema's aren't compatible
+interface CombinedWebViewProps extends WebViewProps {
+  source: any;
+}
+
+export type WebViewType = new (props: CombinedWebViewProps) =>
+  | ReactNativeWebView
+  | CommunityWebView;
